91240500df93097e5d24eb2e2b4ff359bfa06ecb
BraulioCalix55 <djbrzhn1@hotmail.com>
2018-08-10 14:28:07 -0500

    Creacion de clases

--- Lab4_JosueVidalyBraulioCalix/src/lab4_josuevidalybrauliocalix/Lab4_JosueVidalyBraulioCalix.java
+++ Lab4_JosueVidalyBraulioCalix/src/lab4_josuevidalybrauliocalix/Lab4_JosueVidalyBraulioCalix.java
@@ -16,6 +16,58 @@ public class Lab4_JosueVidalyBraulioCalix {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        String[][] Mat = gener();
+        imprimat(Mat);
+        boolean vivo = true;
+        int turno = 1;//1 PARA JUGADOR 1, 2 PARA JUGADOR 2
+        while (vivo == true) {
+            if (turno == 1) {
+                System.out.println("jugador 1");
+                turno++;
+                //ingresar metodos
+            } else if (turno == 2) {
+                System.out.println("jugador 2");
+                turno--;
+                //ingresar metodos
+                 vivo=false;
+            }
+          
+        }
     }
-    
+
+    public static String[][] gener() {
+        String[][] mat = new String[10][10];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                mat[i][j] = "   ";
+            }
+        }
+        mat[0][2] = " R ";
+        mat[0][7] = " R ";
+        mat[1][4] = " S ";
+        mat[1][5] = " S ";
+        mat[1][1] = " Z ";
+        mat[1][8] = " Z ";
+        mat[3][2] = " Z ";
+        mat[3][8] = " Z ";
+        mat[9][0] = "PM ";
+        mat[9][2] = "PM ";
+        mat[9][3] = "EV0";
+        mat[9][4] = "EV1";
+        mat[9][5] = "EV2";
+        mat[9][6] = "EV0";
+        mat[9][7] = " PM";
+        mat[9][9] = " PM";
+        return mat;
+    }
+
+    public static void imprimat(String[][] mat) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                System.out.print("[" + mat[i][j] + "]");
+            }
+            System.out.println("");
+        }
+    }
 }
